830e25b7cf5778249e2d0a04b6d5b0ea68d7c00d
max1620 <x35713712@gmail.com>
2023-10-21 15:21:11 +0800

    second

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -29,15 +29,47 @@ public class    MainActivity extends AppCompatActivity {
         btn_paper = findViewById(R.id.btn_paper);
         btn_mora = findViewById(R.id.btn_mora);
 
-        btn_mora.setOnLongClickListener(view ->{
-            if(ed_name.length()<1){
+        btn_mora.setOnClickListener(view -> {
+            if(ed_name.length() < 1){
                 tv_text.setText("請輸入玩家姓名");
-            }else
+            }else{
                 tv_name.setText(String.format("名字\n%s" , ed_name.getText().toString()));
 
                 if(btn_scissor.isChecked()){
-                    
+                    tv_mmora.setText("我方出拳\n剪刀");
+                } else if (btn_stone.isChecked()){
+                    tv_mmora.setText("我方出拳\n石頭");
+                } else {
+                    tv_mmora.setText("我方出拳\n布");
                 }
-        });
+
+                int computer_random = (int) (Math.random() * 3);
+
+                if(computer_random == 0){
+                    tv_cmora.setText("電腦出拳\n剪刀");
+                }else if (computer_random == 1){
+                    tv_cmora.setText("電腦出拳\n石頭");
+                }else {
+                    tv_cmora.setText("電腦出拳\n布");
+                }
+
+                if((btn_scissor.isChecked() && computer_random == 2) ||
+                        (btn_stone.isChecked() && computer_random == 0) ||
+                        (btn_paper.isChecked() && computer_random == 1)){
+                    tv_winner.setText("勝利者\n" +ed_name.getText().toString());
+                    tv_text.setText("恭喜您獲勝了!!!");
+                } else if ((btn_scissor.isChecked() && computer_random == 1) ||
+                        (btn_stone.isChecked() && computer_random == 2) ||
+                        (btn_paper.isChecked() && computer_random == 0)){
+                    tv_winner.setText("勝利者\n電腦");
+                    tv_text.setText("可惜，電腦獲勝了!");
+                }else{
+                    tv_winner.setText("勝利者\n平手");
+                    tv_text.setText("平局，請在試一次!");
+                }
+
+            }
+
+        } );
     }
 }
